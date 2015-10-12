9e7e0da3a298b10632c5a7d8899f5d3db2059b8e
Matheus Azzi <matheuslazzi@gmail.com>
2015-10-12 00:09:55 -0300

    Update .eslintrc

--- specs/components/back-to-top.spec.js
+++ specs/components/back-to-top.spec.js
@@ -1,31 +1,31 @@
-import $ from 'vendor/jquery/dist/jquery';
-import BackToTop from 'scripts/components/back-to-top';
+import $ from 'vendor/jquery/dist/jquery'
+import BackToTop from 'scripts/components/back-to-top'
 
 describe('BackToTop', () => {
   let button = $('<button />'),
     body = $('body'),
-    elem;
+    elem
 
   beforeEach(() => {
     body.append(
       `<div class="container" style="height:100px;overflow:auto;">
         <div style="height:1000px;" />
       </div>`
-    );
+    )
 
-    elem = $('.container');
-    button.backToTop({ elem: elem, scrollSpeed: 50 });
-  });
+    elem = $('.container')
+    button.backToTop({ elem: elem, scrollSpeed: 50 })
+  })
 
   describe('When click', () => {
     it('scrolls to top of the window', (done) => {
-      elem.scrollTop(500);
-      button.trigger('click');
+      elem.scrollTop(500)
+      button.trigger('click')
 
       setTimeout(function() {
-        expect(elem.scrollTop()).toEqual(0);
-        done();
-      }, 100);
-    });
-  });
-});
+        expect(elem.scrollTop()).toEqual(0)
+        done()
+      }, 100)
+    })
+  })
+})
--- specs/modules/home.spec.js
+++ specs/modules/home.spec.js
@@ -1,26 +1,26 @@
-import Home from 'scripts/modules/home';
+import Home from 'scripts/modules/home'
 
 describe('Home', () => {
-  let subject;
+  let subject
 
   beforeEach(() => {
-    subject = new Home();
-  });
+    subject = new Home()
+  })
 
   describe('#initialize', () => {
     it('call #sayHello', () => {
-      spyOn(subject, 'sayHello');
-      subject.initialize();
+      spyOn(subject, 'sayHello')
+      subject.initialize()
 
-      expect(subject.sayHello).toHaveBeenCalled();
-      subject.sayHello.calls.reset();
-    });
-  });
+      expect(subject.sayHello).toHaveBeenCalled()
+      subject.sayHello.calls.reset()
+    })
+  })
 
   describe('#sayHello', () => {
     it('return module message', () => {
-      subject.message = 'Foo';
-      expect(subject.sayHello()).toMatch('Foo');
-    });
-  });
-});
+      subject.message = 'Foo'
+      expect(subject.sayHello()).toMatch('Foo')
+    })
+  })
+})
